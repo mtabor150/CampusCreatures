431ddc7f40e3a78bd21fbfeb3442ad536a3d48d5
Phillip <pmarti28@slu.edu>
2014-02-04 19:02:49 -0600

    Added a prototypical menu with Game, settings, and profile buttons.

--- CampusCreatures/src/com/example/campuscreatures/MainActivity.java
+++ CampusCreatures/src/com/example/campuscreatures/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.campuscreatures;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,21 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void goToGame(View view) {
+		Intent i = new Intent(this, GameActivity.class);
+		startActivity(i);
+	}
+	
+	public void goToSettings(View view) {
+		Intent i = new Intent(this, SettingsActivity.class);
+		startActivity(i);
+	}
+	
+	public void goToProfile(View view){
+		Intent i = new Intent(this, ProfileActivity.class);
+		startActivity(i);
 	}
 
 }
