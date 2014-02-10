5840a40b9f8a717bd1117fe46ffd8f7a621a55fb
Phillip <pmarti28@slu.edu>
2014-02-10 00:45:03 -0600

    attempting to convert changes on upstream repo to local repo

--- src/com/example/campuscreatures/MapActivity.java
+++ src/com/example/campuscreatures/MapActivity.java
@@ -2,28 +2,21 @@ package com.example.campuscreatures;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
-public class GameActivity extends Activity {
+public class MapActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_game);
+		setContentView(R.layout.activity_map);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.game, menu);
+		getMenuInflater().inflate(R.menu.map, menu);
 		return true;
-	}
-	
-	public void goToTrapCreatures(View view) {
-		Intent i = new Intent(this, TrapCreaturesActivity.class);
-		startActivity(i);
 	}
 
 }
