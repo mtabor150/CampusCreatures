a1de65976cc42368d51ef0b163fe94050ee906f7
James <jwessel3@slu.edu>
2014-02-20 17:20:18 -0600

    Finalized MainActivity Layout Created Temporarily Blank CreatureStats Activity All activites linked together.

--- gen/com/example/campuscreatures/R.java
+++ gen/com/example/campuscreatures/R.java
@@ -24,39 +24,73 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Explore=0x7f080009;
-        public static final int Hunt=0x7f080008;
-        public static final int Recon=0x7f080007;
-        public static final int action_settings=0x7f08000a;
-        public static final int button1=0x7f080006;
-        public static final int gameButton=0x7f080002;
+        public static final int Explore=0x7f080024;
+        public static final int Hunt=0x7f080023;
+        public static final int Recon=0x7f080022;
+        public static final int action_settings=0x7f080025;
+        public static final int button1=0x7f080021;
+        public static final int gameButton=0x7f080006;
+        public static final int imageButton1=0x7f080019;
+        public static final int imageButton2=0x7f08001a;
+        public static final int imageButton3=0x7f08001c;
+        public static final int imageButton4=0x7f08001d;
+        public static final int imageButton5=0x7f08001f;
+        public static final int imageButton6=0x7f080020;
+        public static final int imageView1=0x7f080002;
         public static final int mapButton=0x7f080001;
-        public static final int profileButton=0x7f080005;
-        public static final int settingsButton=0x7f080004;
+        public static final int profileButton=0x7f080007;
+        public static final int scrollView1=0x7f080009;
+        public static final int settingsButton=0x7f080008;
+        public static final int tableRow1=0x7f080018;
+        public static final int tableRow2=0x7f08001b;
+        public static final int tableRow3=0x7f08001e;
         public static final int textView1=0x7f080003;
+        public static final int textView10=0x7f080010;
+        public static final int textView11=0x7f080011;
+        public static final int textView12=0x7f080012;
+        public static final int textView13=0x7f080013;
+        public static final int textView14=0x7f080014;
+        public static final int textView15=0x7f080015;
+        public static final int textView16=0x7f080016;
+        public static final int textView17=0x7f080017;
+        public static final int textView2=0x7f080004;
+        public static final int textView3=0x7f080005;
+        public static final int textView4=0x7f08000a;
+        public static final int textView5=0x7f08000b;
+        public static final int textView6=0x7f08000c;
+        public static final int textView7=0x7f08000d;
+        public static final int textView8=0x7f08000e;
+        public static final int textView9=0x7f08000f;
         public static final int trapCreaturesButton=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_game=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_map=0x7f030002;
-        public static final int activity_profile=0x7f030003;
-        public static final int activity_settings=0x7f030004;
-        public static final int activity_trap_creatures=0x7f030005;
+        public static final int activity_creature_stats=0x7f030000;
+        public static final int activity_game=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_map=0x7f030003;
+        public static final int activity_profile=0x7f030004;
+        public static final int activity_settings=0x7f030005;
+        public static final int activity_trap_creatures=0x7f030006;
     }
     public static final class menu {
-        public static final int game=0x7f070000;
-        public static final int hello_google_maps=0x7f070001;
-        public static final int main=0x7f070002;
-        public static final int map=0x7f070003;
-        public static final int profile=0x7f070004;
-        public static final int settings=0x7f070005;
-        public static final int trap_creatures=0x7f070006;
+        public static final int creature_stats=0x7f070000;
+        public static final int creature_stats_activitiy=0x7f070001;
+        public static final int game=0x7f070002;
+        public static final int hello_google_maps=0x7f070003;
+        public static final int main=0x7f070004;
+        public static final int map=0x7f070005;
+        public static final int profile=0x7f070006;
+        public static final int settings=0x7f070007;
+        public static final int trap_creatures=0x7f070008;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int button_Map=0x7f05000a;
+        public static final int button_trapCreatures=0x7f050009;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_creature_stats=0x7f05000c;
+        public static final int title_activity_creature_stats_activitiy=0x7f05000b;
         public static final int title_activity_game=0x7f050003;
         public static final int title_activity_hello_google_maps=0x7f050008;
         public static final int title_activity_map=0x7f050007;
