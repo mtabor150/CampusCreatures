35e42ea4aaca136f956c6a67e85ef1637aff3380
mtabor150 <mtabor150@gmail.com>
2014-03-14 14:04:22 -0500

    merging

--- src/com/example/campuscreatures/BattleActivity.java
+++ src/com/example/campuscreatures/BattleActivity.java
@@ -13,16 +13,16 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import battleMechanics.Battle;
 import battleMechanics.BattleAction;
-import battleMechanics.BattleCreature;
+import battleMechanics.Creature;
 
 public class BattleActivity extends Activity {
-	
+
 	//one battle, two creatures, one boolean to track the player's turn
 	private Battle currentBattle;
-	private BattleCreature player;
-	private BattleCreature opponent;
+	private Creature player;
+	private Creature opponent;
 	private boolean isPlayerTurn;
-	
+
 	//three modifiable TextViews for player and opponent
 	//those for player:
 	private TextView playerHealth;
@@ -32,15 +32,15 @@ public class BattleActivity extends Activity {
 	private TextView oppHealth;
 	private TextView oppLevel;
 	private TextView oppXP;
-	
+
 	private TextView messageTextView;
-	
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_battle);
-		
+
 		isPlayerTurn = true;
 		//create sample battleActions, creatures, and Battle
 		//create battleActions
@@ -53,33 +53,27 @@ public class BattleActivity extends Activity {
 		simpleActions.add(heal);
 		simpleActions.add(burn);
 		simpleActions.add(push);
-		
-<<<<<<< HEAD
+
 		//create sample creatures
 		player = new Creature("Phil",1,10,10,0,simpleActions);
 		opponent = new Creature("Mark",1,10,10,0,simpleActions);
-=======
-		//create creatures
-		player = new BattleCreature("Phil",1,10,10,0,simpleActions);
-		opponent = new BattleCreature("Mark",1,10,10,0,simpleActions);
->>>>>>> 42b0a3eeda205f21d2c19e3347694eeb2ddf0ec9
-		
+
 		//create the battle for this activity
 		currentBattle = new Battle(player,opponent);
 
-		
+
 		//the following are the TableRows from the layout to be referenced
 		TableRow playerTitleTR= (TableRow) this.findViewById(R.id.playerTitle);
 		TableRow playerHealthTR = (TableRow) this.findViewById(R.id.playerHealth);
 		TableRow playerLevelTR = (TableRow) this.findViewById(R.id.playerLevel);
 		TableRow playerXPTR = (TableRow) this.findViewById(R.id.playerExperience);
-		
+
 		TableRow oppTitleTR = (TableRow) this.findViewById(R.id.oppTitle);
 		TableRow oppHealthTR = (TableRow) this.findViewById(R.id.oppHealth);
 		TableRow oppLevelTR = (TableRow) this.findViewById(R.id.oppLevel);
 		TableRow oppXPTR = (TableRow) this.findViewById(R.id.oppExperience);
-		
-		
+
+
 		//create player textViews to be added to tableRows
 		TextView playerTitle = new TextView(this);
 		playerHealth = new TextView(this);
@@ -93,7 +87,7 @@ public class BattleActivity extends Activity {
 		playerHealthTR.addView(playerHealth);
 		playerLevelTR.addView(playerLevel);
 		playerXPTR.addView(playerXP);
-		
+
 		//create opponent TextViews to be added to TableRows
 		TextView oppTitle = new TextView(this);
 		oppHealth = new TextView(this);
@@ -107,7 +101,7 @@ public class BattleActivity extends Activity {
 		oppHealthTR.addView(oppHealth);
 		oppLevelTR.addView(oppLevel);
 		oppXPTR.addView(oppXP);
-		
+
 		//create message TextView
 		messageTextView = (TextView) this.findViewById(R.id.battleMessage);
 		messageTextView.setText("Fight!");
@@ -119,11 +113,11 @@ public class BattleActivity extends Activity {
 		getMenuInflater().inflate(R.menu.battle, menu);
 		return true;
 	}
-	
+
 	//one function to implement each of the 
 	private void battleAction(int i) {
 		System.out.println("got here " + i);
-		
+
 		//if it is player's turn, allow the action
 		if (isPlayerTurn) {
 			isPlayerTurn = false;
@@ -138,29 +132,29 @@ public class BattleActivity extends Activity {
 		System.out.println(player.getCurrentHealth());
 		playerHealth.setText("Health: " + player.getCurrentHealth());
 		oppHealth.setText("Health: " + opponent.getCurrentHealth());
-		
+
 		//if round == 0, battle is over, set the battle message
 		if(currentBattle.getRound() == 0) {
 			setMessage("Game Over! \nDoesn't matter who won, phil >> mark.");
 		}
 	}
-	
+
 	public void battleAction1(View view) {
 		this.battleAction(0);
 	}
-	
+
 	public void battleAction2(View view) {
 		battleAction(1);
 	}
-	
+
 	public void battleAction3(View view) {
 		battleAction(2);
 	}
-	
+
 	public void battleAction4(View view) {
 		battleAction(3);
 	}
-	
+
 	private void setMessage(String message) {
 		messageTextView.setText(message);
 	}
