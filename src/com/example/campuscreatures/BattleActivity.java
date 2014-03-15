c34598b7c9d719965d0e294ab19a37487d6a8539
Phillip <pmarti28@slu.edu>
2014-03-14 21:45:27 -0500

    modified the battle mechanics to have a better feel

--- src/com/example/campuscreatures/BattleActivity.java
+++ src/com/example/campuscreatures/BattleActivity.java
@@ -1,13 +1,15 @@
 package com.example.campuscreatures;
 
 import java.util.ArrayList;
-
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.RemoteViews;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -15,6 +17,7 @@ import battleMechanics.Battle;
 import battleMechanics.BattleAction;
 import battleMechanics.BattleCreature;
 
+@SuppressLint("NewApi")
 public class BattleActivity extends Activity {
 
 	//one battle, two creatures, one boolean to track the player's turn
@@ -25,15 +28,21 @@ public class BattleActivity extends Activity {
 
 	//three modifiable TextViews for player and opponent
 	//those for player:
+	private TextView playerTitle;
 	private TextView playerHealth;
 	private TextView playerLevel;
 	private TextView playerXP;
+	private TextView playerTurn;
 	//those for opponent:
+	private TextView oppTitle;
 	private TextView oppHealth;
 	private TextView oppLevel;
 	private TextView oppXP;
+	private TextView oppTurn;
 
 	private TextView messageTextView;
+	
+	final Activity activity = this;
 
 
 	@Override
@@ -46,17 +55,27 @@ public class BattleActivity extends Activity {
 		//create battleActions
 		BattleAction kick = new BattleAction("kick",1,0,10);
 		BattleAction heal = new BattleAction("heal",0,2,10);
-		BattleAction burn = new BattleAction("burn",5,0,2);
+		BattleAction burn = new BattleAction("burn",2,0,5);
 		BattleAction push = new BattleAction("push",2,0,5);
-		ArrayList<BattleAction> simpleActions = new ArrayList();
-		simpleActions.add(kick);
-		simpleActions.add(heal);
-		simpleActions.add(burn);
-		simpleActions.add(push);
-
+		BattleAction intimidate = new BattleAction("intimidate",1,0,10);
+		
+		ArrayList<BattleAction> simpleActions1 = new ArrayList();
+		simpleActions1.add(kick);
+		simpleActions1.add(heal);
+		simpleActions1.add(burn);
+		simpleActions1.add(push);
+		
+		ArrayList<BattleAction> simpleActions2 = new ArrayList();
+		simpleActions2.add(kick);
+		simpleActions2.add(heal);
+		simpleActions2.add(burn);
+		simpleActions2.add(intimidate);
+		
 		//create sample creatures
-		player = new BattleCreature("Phil",1,10,10,0,simpleActions);
-		opponent = new BattleCreature("Mark",1,10,10,0,simpleActions);
+		player = new BattleCreature("Phil",1,10,10,0,simpleActions1);
+		simpleActions2.remove(3);
+		simpleActions2.add(intimidate);
+		opponent = new BattleCreature("Mark",1,10,10,0,simpleActions2);
 
 		//create the battle for this activity
 		currentBattle = new Battle(player,opponent);
@@ -67,44 +86,57 @@ public class BattleActivity extends Activity {
 		TableRow playerHealthTR = (TableRow) this.findViewById(R.id.playerHealth);
 		TableRow playerLevelTR = (TableRow) this.findViewById(R.id.playerLevel);
 		TableRow playerXPTR = (TableRow) this.findViewById(R.id.playerExperience);
+		TableRow playerTurnTR = (TableRow) this.findViewById(R.id.playerTurn);
 
 		TableRow oppTitleTR = (TableRow) this.findViewById(R.id.oppTitle);
 		TableRow oppHealthTR = (TableRow) this.findViewById(R.id.oppHealth);
 		TableRow oppLevelTR = (TableRow) this.findViewById(R.id.oppLevel);
 		TableRow oppXPTR = (TableRow) this.findViewById(R.id.oppExperience);
+		TableRow oppTurnTR = (TableRow) this.findViewById(R.id.oppTurn);
 
 
 		//create player textViews to be added to tableRows
-		TextView playerTitle = new TextView(this);
+		playerTitle = new TextView(this);
 		playerHealth = new TextView(this);
 		playerLevel = new TextView(this);
 		playerXP = new TextView(this);
+		playerTurn = new TextView(this);
 		playerTitle.setText(player.getTitle());
 		playerHealth.setText("Health: " + player.getCurrentHealth());
 		playerLevel.setText("Level: " + player.getLevel());
 		playerXP.setText("Experience: " + player.getCreatureExperience());
+		playerTurn.setText("^");
 		playerTitleTR.addView(playerTitle);
 		playerHealthTR.addView(playerHealth);
 		playerLevelTR.addView(playerLevel);
 		playerXPTR.addView(playerXP);
+		playerTurnTR.addView(playerTurn);
 
 		//create opponent TextViews to be added to TableRows
-		TextView oppTitle = new TextView(this);
+		oppTitle = new TextView(this);
 		oppHealth = new TextView(this);
 		oppLevel = new TextView(this);
 		oppXP = new TextView(this);
+		oppTurn = new TextView(this);
 		oppTitle.setText(opponent.getTitle());
 		oppHealth.setText("Health: " + opponent.getCurrentHealth());
 		oppLevel.setText("Level: " + opponent.getLevel());
 		oppXP.setText("Experience: " + opponent.getCreatureExperience());
+		oppTurn.setText("");
 		oppTitleTR.addView(oppTitle);
 		oppHealthTR.addView(oppHealth);
 		oppLevelTR.addView(oppLevel);
 		oppXPTR.addView(oppXP);
+		oppTurnTR.addView(oppTurn);
 
 		//create message TextView
 		messageTextView = (TextView) this.findViewById(R.id.battleMessage);
 		messageTextView.setText("Fight!");
+		
+		
+		//change names of battleAction buttons
+		adjustBattleActionButtons();
+		
 	}
 
 	@Override
@@ -113,29 +145,69 @@ public class BattleActivity extends Activity {
 		getMenuInflater().inflate(R.menu.battle, menu);
 		return true;
 	}
+	
+	//change buttons to reflect names of attacks the current creature
+	private void adjustBattleActionButtons() {
+		//figure out which creature has it's turn and modify the buttons to reflect its moves
+		BattleCreature tempCreature;
+		if(currentBattle.isPlayerTurn()) {
+			tempCreature = player;
+		}
+		else {
+			tempCreature = opponent;
+		}
+		for (int i = 0; i< 4; i++) {
+			Button tempButton = (Button) activity.findViewById(R.id.button1);
+			
+			switch(i) {
+			case 0: tempButton = (Button) this.findViewById(R.id.attack1);
+			break;
+			case 1: tempButton = (Button) this.findViewById(R.id.attack2);
+			break;
+			case 2: tempButton = (Button) this.findViewById(R.id.attack3);
+			break;
+			case 3: tempButton = (Button) this.findViewById(R.id.attack4);
+			break;
+			default: break;
+			}
+			//grab the layout, then set the text of the Button called R.id.Counter:
+			//RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.activity_battle);
+			//remoteViews.setTextViewText(R.id.button1, "hole");
+			tempButton.setText(tempCreature.getBattleActions().get(i).getTitle());
+		}
+		
+	}
 
-	//one function to implement each of the 
+	//one function to implement each of the buttons
 	private void battleAction(int i) {
-		System.out.println("got here " + i);
-
-		//if it is player's turn, allow the action
+		/*
+		 * if it is player's turn, allow the action
+		 * otherwise, have the opponent do its action for that button.
+		 * Until a simple AI is developed we will continue this implementation
+		 */
 		if (isPlayerTurn) {
 			isPlayerTurn = false;
 			currentBattle.playerAction(i);
 		}
-		//Otherwise, have the opponent do its action
-		//Until a simple AI is developed we will continue this implementation
 		else {
 			isPlayerTurn = true;
 			currentBattle.oppAction(i);
 		}
-		System.out.println(player.getCurrentHealth());
 		playerHealth.setText("Health: " + player.getCurrentHealth());
 		oppHealth.setText("Health: " + opponent.getCurrentHealth());
 
 		//if round == 0, battle is over, set the battle message
 		if(currentBattle.getRound() == 0) {
-			setMessage("Game Over! \nDoesn't matter who won, phil >> mark.");
+			if(player.getCurrentHealth() > 0) {
+				setMessage(player.getTitle() + " won!");
+			}
+			else {
+				setMessage(opponent.getTitle() + " won!");
+			}
+		}
+		else{
+			adjustBattleActionButtons();
+			updateTurnPointer();
 		}
 	}
 
@@ -155,8 +227,21 @@ public class BattleActivity extends Activity {
 		battleAction(3);
 	}
 
+	//for the message textView at the bottom of the page, this function sets the value
 	private void setMessage(String message) {
 		messageTextView.setText(message);
+	}
+	
+	//change the "^" to the proper creature. "^" points to the creature whose turn it is
+	private void updateTurnPointer() {
+		if(currentBattle.isPlayerTurn()) {
+			playerTurn.setText("^");
+			oppTurn.setText("");
+		}
+		else {
+			playerTurn.setText("");
+			oppTurn.setText("^");
+		}
 	}
 
 }
