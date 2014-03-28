9ac6b6ce1c59f6c5eee771d4dbdcee97ebb3f306
Mr-Runtime <jwessel3@slu.edu>
2014-03-27 21:43:06 -0500

    Finally pushing UI with Swipe Views

--- src/campuscreatures/main/MainActivity.java
+++ src/campuscreatures/main/MainActivity.java
@@ -10,27 +10,80 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity {
+	
+	
+	private ViewPager viewPager;
+    private MainPagerAdapter mAdapter;
+   
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		//setup
+		mAdapter = new MainPagerAdapter(getSupportFragmentManager());
+		
+		viewPager = (ViewPager)findViewById(R.id.pager);
+		viewPager.setAdapter(mAdapter);
+		viewPager.setCurrentItem(1);
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int arg0) {
+				
+				
+			}
+			
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		
+		
+		
+		
 		DatabaseHelper database = new DatabaseHelper(this);
-
 		/*
 		 * test creatures database, and tests for all functions (add, delete, getAll)
-		 * ID,Name, Region, District, Type, Health, Magic, Attack, Defense, Speed, Moves Per Turn, Experience, Level
-		 * 
 		 */
-		database.addCreature(new Creatures("SabortoothTabor", "Ritter Hall", "Saint Louis University", "earth", 1, 2, 3, 4, 5, 6, 7, 8));
-		database.addCreature(new Creatures("Desi Djinn ", "Simon Rec", "Saint Louis University", "earth",  21, 22, 23, 24, 25, 26, 27, 28));
+		
+		// ID,Name, Region, District, Type, Health, Magic, Attack, Defense, Speed, Moves Per Turn, Experience, Level
+
+		database.addCreature(new Creatures("Sabortooth Tabor", "Ritter Hall", "Saint Louis University", "earth", 10, 10, 10, 10, 10, 10, 10, 10));
+		database.addCreature(new Creatures("Desi Djinn ", "Simon Rec", "Saint Louis University", "earth",  10, 10, 10, 10, 10, 10, 10, 10));
+		database.addCreature(new Creatures("Philanderer", "Ritter Hall", "Saint Louis University", "earth", 10, 10, 10, 10, 10, 10, 10, 10));
+		database.addCreature(new Creatures("Weazel Man", "Pius Library", "Saint Louis University", "earth", 10, 10, 10, 10, 10, 10, 10, 10));
+		database.addCreature(new Creatures("Adam the Intern", "Pius Library", "Saint Louis University", "earth", 10, 10, 10, 10, 10, 10, 10, 10));
 		
 		//get all Creatures
 		//List<Creatures> list = database.getAllCreatures();
-		database.close();
+		
+		//delete one creature
+		//cause a crash, NEED TO FIX INSERT (creature) METHOD
+		//database.deleteCreature(list.get(4)); //should delete Adam the Intern
+		
+		//get all creatures
+		//database.getAllCreatures();
+
+
 	}
 
 	@Override
@@ -59,7 +112,6 @@ public class MainActivity extends Activity {
 		Intent i = new Intent(this, CreatureStatsActivity.class);
 		startActivity(i);
 	}
-	
-	
 
 }
+
