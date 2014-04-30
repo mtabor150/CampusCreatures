c214d720e578854927c0523ff7fa75863622ffba
mtabor150 <mtabor150@gmail.com>
2014-04-29 22:57:51 -0500

    Fixed zone so it sorts quad points clockwise

--- src/campuscreatures/location/Zone.java
+++ src/campuscreatures/location/Zone.java
@@ -3,12 +3,12 @@ package campuscreatures.location;
 /* Zones are either circles designated with a center and a radius
  * or they are convex quadrilaterals designated by 4 points.  If the
  * zone is a quadrilateral the constructor will sort the points so 
- * that the two points farthest from each other are in the point array
- * at index 0 and 2.  This sorts the points into order.
+ * that they are in clockwise order in the array of points.
  */
 
 public class Zone {
-
+	
+	private Location barycenter = null;
 	private Location[] points = null;
 	private int numPoints = 0;
 	private double radius = 0;
@@ -17,78 +17,69 @@ public class Zone {
 		numPoints = 1;
 		points = new Location[numPoints];
 		points[0] = center;
+		barycenter = center;
 		radius = rad;
+	}
+	
+	public Zone(Location center, Location outerpoint){
+		numPoints = 1;
+		points = new Location[numPoints];
+		points[0] = center;
+		barycenter = center;
+		radius = pointDistance(center, outerpoint);
 	}
 
 	public Zone(Location alpha, Location beta, Location gamma, Location delta) {
 		numPoints = 4;
 		points = new Location[numPoints];
-
-		double ab, ac, ad, bc, bd, cd;
-		ab = pointDistance(alpha, beta);
-		ac = pointDistance(alpha, gamma);
-		ad = pointDistance(alpha, delta);
-		bc = pointDistance(beta, gamma);
-		bd = pointDistance(beta, delta);
-		cd = pointDistance(gamma, delta);
-
-		double[] lines = {ab, ac, ad, bc, bd, cd};
-
-		int maxIndex = 0;
-		for (int i = 0; i < 6; i++) {
-			double newnumber = lines[i];
-			if (newnumber > lines[maxIndex]) {
-				maxIndex = i;
+		setBarycenter(alpha, beta, gamma, delta);
+		Location temp[] = {alpha, beta, gamma, delta};
+		
+		for(int i =0; i<4; i++){
+			double latDiff = temp[i].getLatitude() - barycenter.getLatitude(); 
+			double lonDiff = temp[i].getLongitude() - barycenter.getLongitude();
+			
+			if((latDiff > 0 && lonDiff > 0)||(latDiff > 0 && lonDiff == 0)){
+				points[0] = temp[i];
+			}else if((latDiff < 0 && lonDiff > 0)||(latDiff == 0 && lonDiff > 0)){
+				points[1] = temp[i];
+			}else if((latDiff < 0 && lonDiff < 0)||(latDiff < 0 && lonDiff == 0)){
+				points[2] = temp[i];
+			}else{
+				points[3] = temp[i];
 			}
+			
+		}
+	}
+	
+	
+	public boolean inZone(Location point) {
+
+		boolean inArea = false;
+
+		if (numPoints == 1) {
+			if (pointDistance(points[0], point) <= radius) {
+				inArea = true;
+			}
+		} else if (numPoints == 4){
+			double zoneArea = area(points[0],points[1],points[2],points[3]);
+			double tri1 = area(point, points[0], points[1]);
+			double tri2 = area(point, points[1], points[2]);
+			double tri3 = area(point, points[2], points[3]);
+			double tri4 = area(point, points[3], points[0]);
+			
+			if (zoneArea == (tri1+tri2+tri3+tri4)){
+				inArea = true;
+			} 
 		}
 
-		//put end points of the longest line (a diagonal) in spaces 0 and 2
-		switch (maxIndex) {
-			//ab
-			case 0:
-				points[0] = alpha;
-				points[1] = gamma;
-				points[2] = beta;
-				points[3] = delta;
-				break;
-			//ac
-			case 1:
-				points[0] = alpha;
-				points[1] = beta;
-				points[2] = gamma;
-				points[3] = delta;
-				break;
-			//ad
-			case 2:
-				points[0] = alpha;
-				points[1] = beta;
-				points[2] = delta;
-				points[3] = gamma;
-				break;
-			//bc
-			case 3:
-				points[0] = beta;
-				points[1] = alpha;
-				points[2] = gamma;
-				points[3] = delta;
-				break;
-			//bd
-			case 4:
-				points[0] = beta;
-				points[1] = alpha;
-				points[2] = delta;
-				points[3] = gamma;
-				break;
-			//cd
-			case 5:
-				points[0] = gamma;
-				points[1] = alpha;
-				points[2] = delta;
-				points[3] = beta;
-				break;
-			default:
-				break;
-		}
+		return inArea;
+	}
+	
+	private void setBarycenter(Location alpha, Location beta, Location gamma, Location delta){
+		double lat = (alpha.getLatitude()+beta.getLatitude()+gamma.getLatitude()+delta.getLatitude())/4;
+		double lon = (alpha.getLongitude()+beta.getLongitude()+gamma.getLongitude()+delta.getLongitude())/4;
+		barycenter = new Location(lat, lon);
 	}
 
 	private double pointDistance(Location alpha, Location beta) {
@@ -113,27 +104,5 @@ public class Zone {
 		return ((area(alpha, gamma, beta))+(area(alpha, gamma, delta)));
 	}
 
-	public boolean inZone(Location point) {
-
-		boolean inArea = false;
-
-		if (numPoints == 1) {
-			if (pointDistance(points[0], point) <= radius) {
-				inArea = true;
-			}
-		} else if (numPoints == 4){
-			double zoneArea = area(points[0],points[1],points[2],points[3]);
-			double tri1 = area(point, points[0], points[1]);
-			double tri2 = area(point, points[1], points[2]);
-			double tri3 = area(point, points[2], points[3]);
-			double tri4 = area(point, points[3], points[0]);
-			
-			if (zoneArea == (tri1+tri2+tri3+tri4)){
-				inArea = true;
-			} 
-		}
-
-		return inArea;
-	}
 
 }
