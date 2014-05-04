95b5abf792e344e311a66afb2a0e698b7f545897
Mr-Runtime <jwessel3@slu.edu>
2014-05-03 19:04:17 -0500

    changes that didn't make it into last commit due to merge conflicts

--- src/campuscreatures/battleMechanics/BattleCreature.java
+++ src/campuscreatures/battleMechanics/BattleCreature.java
@@ -271,5 +271,18 @@ public class BattleCreature implements Serializable {
 			}
 		}
 	}
-
+	
+	public boolean equals(BattleCreature creature){
+		if (this.getTitle() == creature.getTitle() &&
+			this.getLevel() == creature.getLevel() &&
+			this.getMaxHealth() == creature.getMaxHealth() &&
+			this.getSpeed() == creature.getSpeed() &&
+			this.getCurrentHealth() == creature.getCurrentHealth() &&
+			this.getCreatureExperience() == creature.getCreatureExperience())
+		{
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
